f109099994b09a4a9addc5e1b5c7300e29199183
syedadil234 <126705339+syedadil234@users.noreply.github.com>
2023-03-10 22:41:26 +0530

    Add files via upload

---
+++ hi/index.php
@@ -0,0 +1,16 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+   <center> <div class="floating-text" data-text="THANK YOU">thank you</div>
+   </center>
+   <!-- <a href="index1.php"></a> -->
+  </body>
+</html>
